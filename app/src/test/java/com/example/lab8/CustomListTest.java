486ac3fc166554433c6f072077576e140ce2957f
Muchen Li <muchen@ualberta.ca>
2022-10-28 14:26:06 -0600

    initialize CustomList in CustomListTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,12 +4,20 @@ package com.example.lab8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-//import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
-    
+    private CustomList list;
+    /**
+     * we create a CustomList object before running
+     */
+    @Before
+    public void createList(){
+        list = new CustomList(null,new ArrayList<City>());
+    }
+
 }
