8a73270e2fbbf2330defe4253d690babba5c2be5
Muchen Li <muchen@ualberta.ca>
2022-10-28 14:28:44 -0600

    add addCityTest without implementing addCity method

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -20,4 +20,15 @@ public class CustomListTest {
         list = new CustomList(null,new ArrayList<City>());
     }
 
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));assertEquals(list.getCount(),listSize + 1);
+    }
 }
