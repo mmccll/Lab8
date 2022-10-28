524fce398299f53dbc12a6ea9e9e41c04defb803
Muchen Li <muchen@ualberta.ca>
2022-10-28 14:41:26 -0600

    implement addCity method

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,11 +13,12 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
     /**
-     * we create a CustomList object before running
+     * create a mocklist for my citylist
+     * @return
      */
-    @Before
-    public void createList(){
-        list = new CustomList(null,new ArrayList<City>());
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
     }
 
     /**
@@ -28,7 +29,9 @@ public class CustomListTest {
      */
     @Test
     public void addCityTest(){
+        list = MockCityList();
         int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SK"));assertEquals(list.getCount(),listSize + 1);
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
     }
 }
